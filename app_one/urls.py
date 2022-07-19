6825c1fb3d292f254f37d5de0a9d42471a01fce7
Verified ✔️ <ryesan58@gmail.com>
2022-07-19 17:52:10 -0400

    SOLO

--- app_one/urls.py
+++ app_one/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index)
+    path('six/<int:my_val>', views.index),
+    path('routed/', views.fromthesix),
+    path('redirected_route', views.redirected_method),
 ]
--- app_one/views.py
+++ app_one/views.py
@@ -1,7 +1,15 @@
-from ast import Return
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+
+from django.http import JsonResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("this is the same as @app.route('/")
+def index(request, my_val):
+    return HttpResponse(f"this is the same as @app.route({my_val}")
+
+def fromthesix(request):
+    return HttpResponse('Veiws from the 6ix')
+
+
+def redirected_method(request):
+    return JsonResponse({"response": "JSON response from redirected_method", "status": True})
